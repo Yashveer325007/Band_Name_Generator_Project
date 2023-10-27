996759e559cc5c69814c2f845a8782ca1933674c
Yashveer Singh Makhloga <119954921+Yashveer325007@users.noreply.github.com>
2023-10-27 19:30:59 +0530

    Create Band_Name_Generator.py

---
+++ Band_Name_Generator.py
@@ -0,0 +1,9 @@
+#Beginning with greeting 
+print("Welcome to your Band Name Generator\n")
+#Asking the user for their city
+city=input("Which city do you belong to?\n")
+#Asking user the name of their pet
+pet=input("What is name of your pet?\n")
+#combining city and pet name to create a name
+print("Your Band Name could be" + " " + pet + city)
+
